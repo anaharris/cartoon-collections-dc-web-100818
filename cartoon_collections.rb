914f4e78b7d555f200c4ac2a2f1f795175f32661
Ana Harris <vana1990@gmail.com>
2018-09-30 01:08:00 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 
 def find_the_cheese(array)
   cheese = ["cheddar", "gouda", "camembert"]
-  if array.include? ("cheddar" || "gouda" || "camembert") == true 
+  if array.include? ("cheddar") || array.include? ("gouda") || array.include? "camembert") == true 
     return array & cheese
   else 
     return nil 
